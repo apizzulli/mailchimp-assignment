a7c6c66c12a9ae633956ba172cdb455af1053fb8
Anthony Pizzulli <anthony.pizzulli@teachingstrategies.com>
2024-02-18 22:56:20 -0500

    fixed naming, added README and custom exception. ready to submit

--- MarkdownToHTMLConverter.java
+++ MarkdownToHTMLConverter.java
@@ -1,17 +1,15 @@
 import java.io.*;
-import java.util.*;
 
-/**
-* This is a Markdown to HTML file converter. The user will input a Markdown file and receive   
-* an HTML file, converted according to the specifications described in 'README.md,' which
-* can be viewed in the browser.
+/*
+* This class is a Markdown to HTML file converter. The user will input a Markdown file and receive   
+* an HTML file as output, converted according to the specifications described in 'README.md.'
 *
 * Run 'java MarkdownProcessor.java' in the command line in the same directory as this file to start.
 *
-* @author Anthony Pizzulli
+* Author: Anthony Pizzulli
 */
 
-public class MarkdownProcessor{
+public class MarkdownToHTMLConverter{
     
     private static final char HEADER_OPEN = '#';
     private static final char LINK_OPEN = '[';
@@ -27,46 +25,49 @@ public class MarkdownProcessor{
     private static final String LINK_CLOSE_TAG = "</a>";
     private static final String HREF_OPEN_TAG = " href=\"";
     private static final String HREF_CLOSE_TAG = "\">";
+    private static final String FILE_FORMAT_ERROR_MESSAGE = "Invalid input file format: ";
 
-    /**
-    * This method receives a line of text in Markdown format and converts it to a line of HTML.
-    * @param String(line): a line of text from the given Markdown file 
+    /*
+    * This method receives a line of text (String) in Markdown format and returns the equivalent HTML String
     */
-    public static String convertMarkdownLineToHTML(String line){
+    public static String convertMarkdownLineToHTML(String line) throws InvalidFileFormatException{
 
-        boolean readingcurrentHeaderLevel = false, readingHeader = false, readingParagraph = false, readingLinkTextContent = false, readingHref = false;
+        boolean readingCurrentHeaderLevel = false, readingHeader = false, readingParagraph = false, readingLinkTextContent = false, readingHref = false;
         String linkTextContent = "", href="", htmlOutputLine = "";
         int currentHeaderLevel = 0;    
 
-        //Read through each line character by character, taking into account any keys such as '#' and pairs of parenthesis.
+        //Read through each line character by character, taking into caccount any keys such as '#' and pairs of parenthesis.
         for (char character : line.toCharArray()) {
             switch (character) {
                 case HEADER_OPEN:
-                    //Nested headers are not valid - just add '#' if already inside of a header element
-                    if(readingHeader){
-                        htmlOutputLine += (character);
+                    //Just add '#' if already inside of a another element
+                    if(readingHeader || readingParagraph || readingHref || readingLinkTextContent){
+                        htmlOutputLine += character;
                         break;
                     }
-                    readingcurrentHeaderLevel = true;
+                    readingCurrentHeaderLevel = true;
                     currentHeaderLevel++;
                     break;
                 case LINK_OPEN:
-                    htmlOutputLine += (LINK_OPEN_TAG);
                     readingLinkTextContent = true;
                     break;
                 case LINK_CLOSE:
                     readingLinkTextContent = false;
                     break;
                 case HREF_OPEN:
+                    //If reading an HREF and the link text has not been closed, the format is invalid
+                    if(readingLinkTextContent){
+                        throw new InvalidFileFormatException(FILE_FORMAT_ERROR_MESSAGE + "'(' missing closing ')'");
+                    }
                     readingHref = true;                            
                     break;
                 case HREF_CLOSE:
                     if (readingHref) {
-                        htmlOutputLine += (HREF_OPEN_TAG + href + HREF_CLOSE_TAG + linkTextContent + LINK_CLOSE_TAG);
+                        htmlOutputLine += LINK_OPEN_TAG + HREF_OPEN_TAG + href + HREF_CLOSE_TAG + linkTextContent + LINK_CLOSE_TAG;
                         readingHref = false;
                     }
                     break;
-                //The default case is executed if the character is not one of the keys '#', '()', or '[].'
+                //The default case is executed if the character is not one of the keys '#', '()', or '[]'
                 default:
                     if (readingLinkTextContent) {
                         linkTextContent += character;
@@ -74,13 +75,13 @@ public class MarkdownProcessor{
                     else if(readingHref){
                         href += character;
                     }
-                    else if (readingcurrentHeaderLevel && character == EMPTY_SPACE) {
-                        htmlOutputLine += (HEADER_OPEN_TAG + currentHeaderLevel + ">");
-                        readingcurrentHeaderLevel = false;
+                    else if (readingCurrentHeaderLevel && character == EMPTY_SPACE) {
+                        htmlOutputLine += HEADER_OPEN_TAG + currentHeaderLevel + ">";
+                        readingCurrentHeaderLevel = false;
                         readingHeader = true;
                     } 
                     else if (!readingHeader && !readingParagraph) {
-                        htmlOutputLine += (PARAGRAPH_OPEN_TAG+character);
+                        htmlOutputLine += PARAGRAPH_OPEN_TAG+character;
                         readingParagraph = true;
                     }
                     else {
@@ -89,7 +90,7 @@ public class MarkdownProcessor{
                     break;
             }
         }
-        //Ensure to close the end of a header or paragraph with the corresponding tag.
+        //Close the end of a header or paragraph with the corresponding tag
         if (readingHeader) {
             htmlOutputLine += (HEADER_CLOSE_TAG + currentHeaderLevel + ">");
         } 
@@ -99,66 +100,80 @@ public class MarkdownProcessor{
         htmlOutputLine += ("\n");
         return htmlOutputLine;
     }
+
     /**
     * The main method handles all input/output, including interaction with the user via the command line
-    * as well as reading/writing from/to files. 
+    * as well as reading/writing from/to files, and handles all exceptions.
     *
     * In addition, it calls the 'convertMarkdownLineToHTML' method with a String read from the input file
     * and writes the returned String into the output file. 
     */
     public static void main(String[] args) throws IOException, FileNotFoundException{
         int numRuns = 0;
-        String inputFileName = "";
+        String inputFileName = "", htmlOutputText = "", outputFileName = "", line = " ";
         BufferedReader markdownInputFile = null;
         BufferedWriter htmlOutputFile = null;
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        //Scanner scanner = new Scanner(System.in);  
+        BufferedReader scanner = new BufferedReader (new InputStreamReader (System.in));
+
         System.out.println("\nWelcome to my Markdown to HTML Converter! Enter 'Q' at any time to exit.");
 
             while(!(inputFileName.equals("Q"))){
                 try{
-                    System.out.print("\nEnter a markdown filename to convert to HTML (extension must be '.md'): ");
-                    inputFileName = scanner.next(); 
+                    System.out.print("\nEnter a markdown file name to convert to HTML (extension must be '.md'): ");
+                    inputFileName = scanner.readLine(); 
                     if(inputFileName.equals("Q")){
                         break;
                     }
                     else if(!isUserInputValid(inputFileName)){
                         continue;
                     }
-                    String outputFileName = "myHTML", line = "";
-                    if(numRuns > 0){
-                        outputFileName += Integer.toString(numRuns);
+                    System.out.print("Enter a name for the output file (if none given, defaults to 'myhtml.html'): ");
+                    outputFileName = scanner.readLine(); 
+                    if(outputFileName.isEmpty()){
+                        outputFileName = "myHTML";
+                        if(numRuns > 0)
+                            outputFileName += Integer.toString(numRuns);
                     }
                     markdownInputFile = new BufferedReader(new FileReader(inputFileName));
-                    htmlOutputFile = new BufferedWriter(new FileWriter(outputFileName += ".html"));
-                    //Process each line in the file one-by-one, ensuring to handle exceptions
+                    //Process each line in the file one-by-one, building the String that will be written to the output file
                     while ((line = markdownInputFile.readLine()) != null) {
                         if(line.length()!=0){
-                            htmlOutputFile.write(convertMarkdownLineToHTML(line));
+                            htmlOutputText += convertMarkdownLineToHTML(line);
                         }
                     }
-                    System.out.println("Success! View your HTML file in " + outputFileName);
-                    markdownInputFile.close();
+                    htmlOutputFile = new BufferedWriter(new FileWriter(outputFileName += ".html"));
+                    htmlOutputFile.write(htmlOutputText);
+                    System.out.println("\nSuccess! View your HTML file in " + outputFileName);
                     htmlOutputFile.close();
                     numRuns++;
                 }catch(FileNotFoundException fileNotFound){
                     System.out.println("\nA file with the given name was not found in this directory.\n");
                 }catch(IOException e){
-                    System.out.println("\nAn error occurred - please try again.\n");
+                    System.out.println(e.getMessage());
+                }catch(InvalidFileFormatException invalidFileFormatException){
+                    System.out.println(invalidFileFormatException.getMessage());
                 }
+                markdownInputFile.close();
             }
         scanner.close();
     }
 
     public static boolean isUserInputValid(String userInput){
-        //If an empty filename/non-.md file is given, re-prompt user.
-        if(userInput == null || userInput.length() == 0){
-            System.out.println("Please input a file name.");
+        //If an empty file name/non-.md file is given, re-prompt user
+        if(userInput == null || userInput.length() == 0 || userInput.length() < 4){
+            System.out.println("Please input a valid file name.");
             return false;
         }else if(!((userInput.substring(userInput.length()-3))).equals(".md")){ 
-            System.out.println("Please input a valid file with extension '.md.'");
+            System.out.println("Please input a file name with extension '.md'");
             return false;
         }
         return true;
     }
 }
 
+class InvalidFileFormatException extends Exception { 
+    public InvalidFileFormatException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,45 @@
+# Markdown => HTML converter
+
+Markdown is a simple syntax used to generate formatted text. Use this command-line tool to convert a given Markdown file to HTML according to the formatting specifics shown below.
+
+## Formatting Specifics
+
+| Markdown                               | HTML                                              |
+| -------------------------------------- | ------------------------------------------------- |
+| `# Heading 1`                          | `<h1>Heading 1</h1>`                              | 
+| `## Heading 2`                         | `<h2>Heading 2</h2>`                              | 
+| `...`                                  | `...`                                             | 
+| `###### Heading 6`                     | `<h6>Heading 6</h6>`                              | 
+| `Unformatted text`                     | `<p>Unformatted text</p>`                         | 
+| `[Link text](https://www.example.com)` | `<a href="https://www.example.com">Link text</a>` | 
+| `Blank line`                           | `Ignored`                                         | 
+
+
+Here are a few sample inputs. 
+
+### Example 1
+
+```
+# Sample Document
+
+Hello!
+
+This is sample markdown for the [Mailchimp](https://www.mailchimp.com) homework assignment.
+```
+
+Converts to the following HTML:
+
+```
+<h1>Sample Document</h1>
+
+<p>Hello!</p>
+
+<p>This is sample markdown for the <a href="https://www.mailchimp.com">Mailchimp</a> homework assignment.</p>
+```
+
+### Example 2
+
+```
 # Header one
 
 Hello there
@@ -10,3 +52,21 @@ What's going on?
 This is a paragraph [with an inline link](http://google.com). Neat, eh?
 
 ## This is a header [with a link](http://yahoo.com)
+```
+
+Converts to the following HTML:
+
+```
+<h1>Header one</h1>
+
+<p>Hello there</p>
+
+<p>How are you?
+What's going on?</p>
+
+<h2>Another Header</h2>
+
+<p>This is a paragraph <a href="http://google.com">with an inline link</a>. Neat, eh?</p>
+
+<h2>This is a header <a href="http://yahoo.com">with a link</a></h2>
+```
